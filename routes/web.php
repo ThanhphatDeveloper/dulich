8eda3cb5bfb3dc9a11dc2ac1a7aeee56d081f39f
Hữu Trung <113398984+Deed-27@users.noreply.github.com>
2023-05-24 10:31:47 +0700

    abc

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/abc', function () {
+    return view('welcome');
+});
+
 Route::get('/blog', function () {
     return 'home';
 });
