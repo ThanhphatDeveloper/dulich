580ec4d6bfb74daaf8c208d523f646ecc726248d
nguyenthanhphat <thanhphatnguyen83@gmail.com>
2023-07-10 10:46:45 +0700

    code new

--- resources/views/customer/thanhtoan-thanhcong.blade.php
+++ resources/views/customer/thanhtoan-thanhcong.blade.php
@@ -1,1 +1,24 @@
-thanh toan thanh cong
+<!-- payment_success.html -->
+    <title>Thanh toán thành công</title>
+ 
+    
+    <div class="container3">
+        <div class="payment-success3">
+            <h1>Thanh toán thành công</h1>
+            <div class="payment-details3">
+                <p><strong>Phương thức thanh toán:</strong> {{ $paymentMethod }}</p>
+                <p><strong>Họ tên:</strong> {{ $fullName }}</p>
+                <p><strong>Email:</strong> {{ $email }}</p>
+                <p><strong>Số điện thoại:</strong> {{ $phone }}</p>
+                <p><strong>Số khách:</strong> {{ $guestCount }}</p>
+                <p><strong>Giới tính:</strong> {{ $gender }}</p>
+                <p><strong>Tour:</strong> {{ $tour }}</p>
+                <p><strong>Khuyến mãi:</strong> {{ $promotion }}</p>
+                <p><strong>Tiền thanh toán:</strong> {{ $amountPaid }}</p>
+                <p><strong>Giá tiền:</strong> {{ $price }}</p>
+            </div>
+            <div class="button-container3">
+                <a href="#" class="button3">Quay lại trang chủ</a>
+            </div>
+        </div>
+    </div>
--- resources/views/customer/thanhtoan-thatbai.blade.php
+++ resources/views/customer/thanhtoan-thatbai.blade.php
@@ -1,1 +1,25 @@
-thanh toan that bai
+<!-- payment_failed.blade.php -->
+
+    <title>Thanh toán thất bại</title>
+
+    <div class="container2">
+        <div class="payment-failed2">
+            <h1>Thanh toán thất bại</h1>
+            <div class="payment-details2">
+                <p><strong>Phương thức thanh toán:</strong> {{ $paymentMethod }}</p>
+                <p><strong>Họ tên:</strong> {{ $fullName }}</p>
+                <p><strong>Email:</strong> {{ $email }}</p>
+                <p><strong>Số điện thoại:</strong> {{ $phone }}</p>
+                <p><strong>Số khách:</strong> {{ $guestCount }}</p>
+                <p><strong>Giới tính:</strong> {{ $gender }}</p>
+                <p><strong>Tour:</strong> {{ $tour }}</p>
+                <p><strong>Khuyến mãi:</strong> {{ $promotion }}</p>
+                <p><strong>Tiền thanh toán:</strong> {{ $amountPaid }}</p>
+                <p><strong>Giá tiền:</strong> {{ $price }}</p>
+            </div>
+            <div class="button-container2">
+                <a href="#" class="button2">Quay lại trang thanh toán</a>
+            </div>
+        </div>
+    </div>
+
