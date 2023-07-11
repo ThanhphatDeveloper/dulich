37b56412c6869e29fd316e22e2bebdbec782577c
Hữu Trung <113398984+Deed-27@users.noreply.github.com>
2023-07-11 20:46:52 +0700

    11_07 20:46

--- app/Http/Controllers/CustomerHomepageController.php
+++ app/Http/Controllers/CustomerHomepageController.php
@@ -4,19 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\CustomerHomepage;
 use App\Models\Tour;
+use App\Models\Blog;
 use App\Http\Requests\StoreCustomerHomepageRequest;
 use App\Http\Requests\UpdateCustomerHomepageRequest;
+use Illuminate\Support\Facades\Storage;
 
 class CustomerHomepageController extends Controller
 {
+    protected function fixImage(Blog $u)
+    {
+        //1 hình bất kì để vào folder public/img
+        if($u->anhdaidien && Storage::disk('public')->exists($u->anhdaidien)){
+            $u->anhdaidien = Storage::url($u->anhdaidien);
+        } else{
+            $u->anhdaidien = '/img/test.gif';
+        }
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
         $lst = Tour::where('trangthai', 1)->orderBy('updated_at','DESC')->take(6)->get();
+        $lst_blog = Blog::where('trangthai', 1)->orderBy('updated_at','DESC')->take(4)->get();
+
+        foreach($lst_blog as $u){
+            $this->fixImage($u);
+        }
         //dd($lst);
-        return view('customer.home-page', ['lst'=>$lst]);
+        return view('customer.home-page', ['lst'=>$lst, 'lst_blog'=>$lst_blog]);
     }
 
     /**
